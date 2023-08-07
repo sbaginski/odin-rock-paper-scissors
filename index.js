649c0095c3e16f2612b58a7a62335542e838e24e
sbaginski <s.baginski@student.uw.edu.pl>
2023-08-07 19:03:59 +0200

    Add the playRound function
    The function playRound takes the choices of both players and returns
    a string that indicates the winner. Also added a helper function that
    converts a string to capital case.

--- index.js
+++ index.js
@@ -9,3 +9,35 @@ function getComputerChoice() {
       return "Scissors";
   }
 }
+
+function toCapital(string) {
+  return string.charAt(0).toUpperCase().concat(string.slice(1).toLowerCase());
+}
+
+function playRound(playerSelection, computerSelection) {
+  let playerSelectionStandardised = toCapital(playerSelection);
+
+  let playerNumber = playerSelectionStandardised === "Rock" ? 0 : 
+    (playerSelectionStandardised === "Paper" ? 1 : 2);
+  let computerNumber = computerSelection === "Rock" ? 0 : (computerSelection === "Paper" ? 1 : 2);
+
+  if (playerNumber === computerNumber) {
+    return "It's a tie!";
+  } else if (playerNumber + computerNumber === 2) {
+    if (playerNumber > computerNumber) {
+      return "You lose! Rock beats scissors!";
+    } else {
+      return "You win! Rock beats scissors!";
+    }
+  } else if (playerNumber > computerNumber) {
+    if (playerNumber === 1) {
+      return "You win! Paper beats rock!";
+    } else {
+      return "You win! Scissors beat paper!";
+    }
+  } else if (playerNumber === 0) {
+    return "You lose! Paper beats rock!";
+  } else {
+    return "You lose! Scissors beat paper!";
+  }
+}
